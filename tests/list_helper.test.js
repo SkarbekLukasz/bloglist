6633625e72305cad9628c638d108789eeeaacb9a
Luke <nycthbre@gmail.com>
2024-06-26 22:16:25 +0200

    Exercise 4.5 completion (helperfunctions)

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -89,3 +89,143 @@ describe('total likes', () => {
     assert.strictEqual(result, 36)
   })
 })
+
+describe('favorite blog', () => {
+  const listOfMany = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422ba71b54a676234d17fb',
+      title: 'TDD harms architecture',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('returns Canonical String Reduction when given a list', () => {
+    const result = listHelper.favoriteBlog(listOfMany)
+    const answer = {
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      likes: 12,
+    }
+
+    assert.deepStrictEqual(result, answer)
+  })
+
+  test('resturns null when given empty list', () => {
+    const list = []
+
+    const result = listHelper.favoriteBlog(list)
+    assert.deepStrictEqual(result, null)
+  })
+
+  test('returns one blog when multiple are the most favorite', () => {
+    const list = [
+      {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      },
+      {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+      },
+      {
+        _id: '5a422b891b54a676234d17fa',
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+        __v: 0
+      },
+      {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 20,
+        __v: 0
+      },
+      {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 20,
+        __v: 0
+      }
+    ]
+
+    const possibleResults = [
+      {
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        likes: 20,
+      },
+      {
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        likes: 20,
+      }
+    ]
+    const result = listHelper.favoriteBlog(list)
+    const matches = possibleResults.filter(possible => possible.title === result.title)[0]
+    assert.deepStrictEqual(matches, result)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,6 +7,22 @@ const totalLikes = (posts) => {
   return posts.reduce((sum, post) => sum+post.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) {
+        return null
+    }
+  const topBlog = blogs.reduce((top, blog) => {
+    return top.likes > blog.likes ? top : blog
+  })
+  const result = {
+    title: topBlog.title,
+    author: topBlog.author,
+    likes: topBlog.likes
+  }
+
+  return result
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
