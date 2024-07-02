9f088985dea2f32e9fa92624f36f0ecc5f1897d1
Luke <nycthbre@gmail.com>
2024-07-02 21:13:48 +0200

    Exercise 4.19 completion (bloglist)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -8,8 +17,12 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const user = await User.findOne()
   const { author, url, title } = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   let blog = new Blog({
     author: author,
     url: url,
--- middleware/exceptionHandler.js
+++ middleware/exceptionHandler.js
@@ -23,6 +23,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'Expected `username` to be unique' })
   } else if(error.name === 'TokenExpiredError') {
     return response.status(401).json({ error: 'token expired' })
+  } else if(error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
   next(error)
 }
